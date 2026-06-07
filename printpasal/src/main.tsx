c7d3fbbf684063270388b33572687abbc3907cfe
Dhiraj Thapa <dhirajthapaparnami@gmail.com>
2026-06-07 18:43:19 +0545

    Added the fix for Promise.withresolves for Win7 Microsoft Edge

--- printpasal/src/main.tsx
+++ printpasal/src/main.tsx
@@ -1,3 +1,14 @@
+if (typeof Promise.withResolvers !== 'function') {
+  Promise.withResolvers = function () {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
+
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
